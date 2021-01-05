62f2e071be10b42f7fc03e695d71dc6f594ddcbd
hariNEzuMI928 <dai.suzuki.829@gmail.com>
2021-01-05 22:42:20 +0900

    eslint適用

--- src/main.js
+++ src/main.js
@@ -1,18 +1,18 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import firebase from 'firebase'
+import {createApp} from 'vue';
+import App from './App.vue';
+import router from './router';
+import firebase from 'firebase';
 
-createApp(App).use(router).mount('#app')
+createApp(App).use(router).mount('#app');
 
-var firebaseConfig = {
-    apiKey: 'AIzaSyCwbzwxmHKpOntyNFabrTXTch53lOHa5tM',
-    authDomain: 'random-lunch-app.firebaseapp.com',
-    projectId: 'random-lunch-app',
-    storageBucket: 'random-lunch-app.appspot.com',
-    messagingSenderId: '852598060019',
-    appId: '1:852598060019:web:e82b4998f05884b6539886',
-    measurementId: 'G-JE6SHRCBGR'
+const firebaseConfig = {
+  apiKey: 'AIzaSyCwbzwxmHKpOntyNFabrTXTch53lOHa5tM',
+  authDomain: 'random-lunch-app.firebaseapp.com',
+  projectId: 'random-lunch-app',
+  storageBucket: 'random-lunch-app.appspot.com',
+  messagingSenderId: '852598060019',
+  appId: '1:852598060019:web:e82b4998f05884b6539886',
+  measurementId: 'G-JE6SHRCBGR',
 };
 
 // Initialize Firebase
